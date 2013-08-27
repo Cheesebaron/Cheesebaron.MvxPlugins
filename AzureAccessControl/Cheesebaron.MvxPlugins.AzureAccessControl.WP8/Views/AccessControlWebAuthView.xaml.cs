cc47169790b0417d6242d6a73af72a7277c1b0d1
Tomasz Cielecki <tomasz@ostebaronen.dk>
2013-08-27 18:35:23 +0200

    Simple WP8 login now works!

--- AzureAccessControl/Cheesebaron.MvxPlugins.AzureAccessControl.WP8/Views/AccessControlWebAuthView.xaml.cs
+++ AzureAccessControl/Cheesebaron.MvxPlugins.AzureAccessControl.WP8/Views/AccessControlWebAuthView.xaml.cs
@@ -1,21 +1,29 @@
-﻿using System.Windows.Navigation;
+﻿using System;
+using System.Windows.Navigation;
+using Cirrious.CrossCore;
+using Cirrious.MvvmCross.Plugins.Messenger;
 using Microsoft.Phone.Controls;
 
 namespace Cheesebaron.MvxPlugins.AzureAccessControl.WindowsPhone.Views
 {
     public partial class AccessControlWebAuthView : PhoneApplicationPage
     {
+        private readonly IMvxMessenger _messageHub;
+        private string _url;
+
         public AccessControlWebAuthView()
         {
             InitializeComponent();
+
+            _messageHub = Mvx.Resolve<IMvxMessenger>();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
 
-            var url = NavigationContext.QueryString["url"];
-            var name = NavigationContext.QueryString["name"];
+            _url = NavigationContext.QueryString["url"];
+            PageName.Text = NavigationContext.QueryString["name"];
 
             BrowserSignInControl.Navigated += SignInWebBrowserControlNavigated;
             BrowserSignInControl.Navigating += SignInWebBrowserControlNavigating;
@@ -23,10 +31,38 @@ namespace Cheesebaron.MvxPlugins.AzureAccessControl.WindowsPhone.Views
             BrowserSignInControl.NavigateToString("<html><head><title></title></head><body></body></html>");
         }
 
-        private void SignInWebBrowserControlScriptNotify(object sender, NotifyEventArgs e) { throw new System.NotImplementedException(); }
+        private async void SignInWebBrowserControlScriptNotify(object sender, NotifyEventArgs e)
+        {
+            BrowserSignInControl.ScriptNotify -= SignInWebBrowserControlScriptNotify;
+            try
+            {
+                var rswt = await RequestSecurityTokenResponse.FromJSONAsync(e.Value);
 
-        private void SignInWebBrowserControlNavigating(object sender, NavigatingEventArgs e) { throw new System.NotImplementedException(); }
+                _messageHub.Publish(rswt == null
+                    ? new RequestTokenMessage(this) {TokenResponse = null}
+                    : new RequestTokenMessage(this) {TokenResponse = rswt});
+            }
+            catch(Exception)
+            {
+                _messageHub.Publish(new RequestTokenMessage(this) { TokenResponse = null });
+            }
+        }
 
-        private void SignInWebBrowserControlNavigated(object sender, NavigationEventArgs e) { throw new System.NotImplementedException(); }
+        private void SignInWebBrowserControlNavigating(object sender, NavigatingEventArgs e)
+        {
+            
+        }
+
+        private void SignInWebBrowserControlNavigated(object sender, NavigationEventArgs e)
+        {
+            if (e.Uri == null && !string.IsNullOrEmpty(_url))
+            {
+                BrowserSignInControl.Navigate(new Uri(_url));
+            }
+            else if (e.Uri != null && string.IsNullOrEmpty(e.Uri.ToString()) && !string.IsNullOrEmpty(_url))
+            {
+                BrowserSignInControl.Navigate(new Uri(_url));
+            }
+        }
     }
 }
