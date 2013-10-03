909ce5c1d95733a1d2a175010791c1423572cab6
Stuart <lodge.stuart@gmail.com>
2013-10-03 19:12:54 +0100

    nuget for Sms

---
+++ nuspec/TouchBootstrapContent/SmsPluginBootstrap.cs.pp
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace $rootnamespace$.Bootstrap
+{
+    public class NetworkPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<Cheesebaron.MvxPlugins.SMS.PluginLoader, Cheesebaron.MvxPlugins.SMS.Touch.Plugin>
+    {
+    }
+}
