374c71e4986caaa2828b56821c1684ba90c6dc68
Stuart <lodge.stuart@gmail.com>
2013-10-04 07:50:20 +0100

    typo fixed - network->sms

--- nuspec/TouchBootstrapContent/SmsPluginBootstrap.cs.pp
+++ nuspec/TouchBootstrapContent/SmsPluginBootstrap.cs.pp
@@ -2,7 +2,7 @@ using Cirrious.CrossCore.Plugins;
 
 namespace $rootnamespace$.Bootstrap
 {
-    public class NetworkPluginBootstrap
+    public class SmsPluginBootstrap
         : MvxLoaderPluginBootstrapAction<Cheesebaron.MvxPlugins.SMS.PluginLoader, Cheesebaron.MvxPlugins.SMS.Touch.Plugin>
     {
     }
