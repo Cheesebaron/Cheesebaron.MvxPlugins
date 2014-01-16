ae9ef7075896a18780346e74c9a89be912b158a5
Tomasz Cielecki <tomasz@ostebaronen.dk>
2014-01-16 15:18:13 +0100

    Fix ACS WP8 sample.

---
+++ Samples/AzureAccessControl.Sample.WindowsPhone/Bootstrap/ModernHttpClientPluginBootstrap.cs
@@ -0,0 +1,9 @@
+﻿using Cirrious.CrossCore.Plugins;
+
+namespace AzureAccessControl.Sample.WindowsPhone.Bootstrap
+{
+    public class ModernHttpClientPluginBootstrap
+        : MvxPluginBootstrapAction<Cheesebaron.MvxPlugins.ModernHttpClient.PluginLoader>
+    {
+    }
+}
