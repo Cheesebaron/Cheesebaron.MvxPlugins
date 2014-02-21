2074470a4467fa83d4a00fe178076c8dd2bab48f
Tomasz Cielecki <tomasz@ostebaronen.dk>
2014-02-21 17:43:05 +0100

    Delay retries, making the handler less aggressive.

--- ModernHttpClient/Core/RetryHandler.cs
+++ ModernHttpClient/Core/RetryHandler.cs
@@ -2,12 +2,14 @@
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.Platform;
 
 namespace Cheesebaron.MvxPlugins.ModernHttpClient
 {
     public class RetryHandler : DelegatingHandler
     {
-        private readonly int _maxRetries = 3;
+        private readonly int _maxRetries;
 
         public RetryHandler(HttpMessageHandler innerHandler, int maxRetries = 3)
             : base(innerHandler) { _maxRetries = maxRetries; }
@@ -16,7 +18,8 @@ namespace Cheesebaron.MvxPlugins.ModernHttpClient
             HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
-            Exception ex = null;
+
+            Exception lastException = null;
             HttpResponseMessage response = null;
             for (var i = 0; i < _maxRetries; i++)
             {
@@ -25,17 +28,20 @@ namespace Cheesebaron.MvxPlugins.ModernHttpClient
                     response = await base.SendAsync(request, cancellationToken);
                     if (response.IsSuccessStatusCode)
                     {
+                        Mvx.Trace(MvxTraceLevel.Diagnostic, "Request was ok after {0} retries...", i);
                         return response;
-                    }  
+                    }
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
-                    ex = e;
+                    lastException = e;
                 }
+                Mvx.Trace(MvxTraceLevel.Diagnostic, "Request failed, retrying...\n{0}", lastException);
+                await Task.Delay(500 + (i * 500), cancellationToken);
             }
 
-            if (ex != null)
-                throw ex;
+            if (lastException != null)
+                throw lastException;
 
             return response;
         }
