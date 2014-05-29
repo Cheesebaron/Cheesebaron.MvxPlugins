7d2b7abbf58c3bcdf7fca4400ce4898b3a91aeb9
Tomasz Cielecki <tomasz@ostebaronen.dk>
2014-05-29 16:00:28 +0200

    WP8 stuff

---
+++ Notifications/WindowsPhone/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using Cirrious.CrossCore;
+using Cirrious.CrossCore.Exceptions;
+using Cirrious.CrossCore.Plugins;
+
+namespace Cheesebaron.MvxPlugins.Notifications
+{
+    public class Plugin 
+        : IMvxConfigurablePlugin
+    {
+        private WPNotificationConfiguration _config;
+        private bool _loaded;
+
+        public void Load()
+        {
+            if (_loaded) return;
+
+            var instance = new WPNotifications();
+
+            if (_config != null)
+            {
+                if(string.IsNullOrEmpty(_config.ChannelName))
+                    instance.ChannelName =
+                        "Cheesebaron.MvxPlugins.NotificationChannel.Tile";
+            }
+
+            Mvx.RegisterSingleton<INotifications>(instance);
+
+            _loaded = true;
+        }
+
+        public void Configure(IMvxPluginConfiguration configuration)
+        {
+            if (configuration != null && !(configuration is WPNotificationConfiguration))
+                throw new MvxException(
+                    "Plugin configuration only supports instances of WPNotificationConfiguration, you provided {0}",
+                    configuration.GetType().Name);
+
+            _config = (WPNotificationConfiguration)configuration;
+        }
+    }
+
+    public class WPNotificationConfiguration
+        : IMvxPluginConfiguration
+    {
+        public string ChannelName { get; set; }
+    }
+}
---
+++ Notifications/WindowsPhone/WPNotifications.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Phone.Notification;
+
+namespace Cheesebaron.MvxPlugins.Notifications
+{
+    public class WPNotifications 
+        : INotifications
+    {
+        private HttpNotificationChannel _notificationChannel;
+
+        public string RegistrationId { get; private set; }
+        public bool IsRegistered { get; private set; }
+
+        public async Task<bool> RegisterForNotifications()
+        {
+            if(string.IsNullOrEmpty(ChannelName))
+                throw new InvalidOperationException(
+                    "ChannelName may not be null or empty prior to calling RegisterForNotifications()");
+
+            if(_notificationChannel == null)
+            {
+                // lets see if we have a push channel already.
+                _notificationChannel = HttpNotificationChannel.Find(ChannelName);
+
+                // still nothing, lets create a new then!
+                if(_notificationChannel == null)
+                {
+                    _notificationChannel = new HttpNotificationChannel(ChannelName);
+
+                    _notificationChannel.Open();
+                }
+
+
+            }
+
+
+
+            await Task.Delay(100);
+            return true;
+        }
+
+        public async Task<bool> UnregisterForNotifications()
+        {
+            await Task.Delay(100);
+            return true;
+        }
+
+        public Func<Task> DidRegisterForNotifications { get; set; }
+        public Func<Task> DidUnregisterForNotifications { get; set; }
+
+        public string ChannelName { get; set; }
+    }
+}
