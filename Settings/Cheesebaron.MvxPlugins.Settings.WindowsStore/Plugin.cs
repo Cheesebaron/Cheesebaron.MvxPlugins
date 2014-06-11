7d9ddccc0d7c1b26dd009c5fd1576b2f559a2548
guillaume-fr <guillaume.moissaing@gmail.com>
2014-06-11 11:28:23 +0200

    Settings plugin for WindowsStore

---
+++ Settings/Cheesebaron.MvxPlugins.Settings.WindowsStore/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using Cheesebaron.MvxPlugins.Settings.Interfaces;
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.Plugins;
+
+namespace Cheesebaron.MvxPlugins.Settings.WindowsStore
+{
+    public class Plugin
+        : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterSingleton<ISettings>(new Settings());
+        }
+    }
+}
