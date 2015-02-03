27ec64a0795b60a27bb0f3e6e032f5554d45f31a
munkii <pat@munkiisoft.com>
2015-02-03 10:23:12 +0000

    WPF and Windows Store Implementations of Settings plugin

---
+++ Settings/Cheesebaron.MvxPlugins.Settings.Wpf/Plugin.cs
@@ -0,0 +1,21 @@
+﻿namespace Cheesebaron.MvxPlugins.Settings.Wpf
+{
+    using Cheesebaron.MvxPlugins.Settings.Interfaces;
+
+    using Cirrious.CrossCore;
+    using Cirrious.CrossCore.Plugins;
+
+    /// <summary>
+    /// Plugin class
+    /// </summary>
+    public class Plugin : IMvxPlugin
+    {
+        /// <summary>
+        /// Loads this instance.
+        /// </summary>
+        public void Load()
+        {
+            Mvx.RegisterSingleton<ISettings>(new Settings());
+        }
+    }
+}
