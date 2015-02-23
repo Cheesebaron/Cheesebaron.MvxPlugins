968f86d6622ff2cda7a82c7a16b0285baeb9ef3c
Emmanuel Huna <hero@stealthlab.net>
2015-02-22 17:22:57 -0800

    FormsPresenters.Sample: Windows 8.1 > added a sample Windows 8.1 project showing that the same MvvmCross viewmodels can be used in Xamarin Forms views and WinRT MvxViews

---
+++ Samples/FormsPresenters.Sample/FormsPresenters.Sample.Store/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.WindowsCommon.Views;
+
+namespace FormsPresenters.Sample.Store.Views
+{
+    public sealed partial class MainView : MvxWindowsPage
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
