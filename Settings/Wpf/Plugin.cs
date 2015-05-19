42a1b910759b2c18873b3333b6220355bacc2452
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-05-19 13:29:10 +0200

    Added Wpf solution for Settings based on #32

---
+++ Settings/Wpf/Plugin.cs
@@ -0,0 +1,50 @@
+﻿using Cheesebaron.MvxPlugins.Settings.Interfaces;
+
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.Exceptions;
+using Cirrious.CrossCore.Plugins;
+
+namespace Cheesebaron.MvxPlugins.Settings.Wpf
+{
+    /// <summary>
+    /// Plugin class
+    /// </summary>
+    public class Plugin : IMvxConfigurablePlugin
+    {
+        private bool _loaded;
+        private WpfCheeseSettingsConfiguration _config;
+
+        /// <summary>
+        /// Loads this instance.
+        /// </summary>
+        public void Load()
+        {
+            if (_loaded) return;
+
+            var fileName = "";
+            if (_config != null)
+                fileName = _config.SettingsFileName;
+
+            var instance = new Settings(fileName);
+            Mvx.RegisterSingleton<ISettings>(instance);
+
+            _loaded = true;
+        }
+
+        public void Configure(IMvxPluginConfiguration configuration)
+        {
+            if (configuration != null && !(configuration is WpfCheeseSettingsConfiguration))
+                throw new MvxException(
+                    "Plugin configuration only supports instances of WpfCheeseSettingsConfiguration, you provided {0}",
+                    configuration.GetType().Name);
+
+            _config = (WpfCheeseSettingsConfiguration)configuration;
+        }
+    }
+
+    public class WpfCheeseSettingsConfiguration
+        : IMvxPluginConfiguration
+    {
+        public string SettingsFileName { get; set; }
+    }
+}
---
+++ Settings/Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Wpf")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wpf")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d7fe3d03-a1e3-4099-aa42-238220460f6c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
