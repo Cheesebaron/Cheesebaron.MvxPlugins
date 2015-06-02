be4f7a5580d9fdc34b0fcc8f5fa60994958814b7
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-06-02 11:38:23 +0200

    Added Visibility bootstrap

---
+++ Samples/Notifications/Droid/Bootstrap/VisibilityPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace Notifications.Sample.Droid.Bootstrap
+{
+    public class VisibilityPluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Visibility.PluginLoader>
+    {
+    }
+}
