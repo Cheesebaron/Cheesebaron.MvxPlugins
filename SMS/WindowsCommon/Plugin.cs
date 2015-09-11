a6c81cb8e92cd88014b89bdfcfb689a8e30e55aa
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-09-11 13:25:53 +0200

    Add SMS plugin for WindowsPhoneStore

---
+++ SMS/WindowsCommon/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using Cirrious.CrossCore;
+using Cirrious.CrossCore.Plugins;
+
+namespace Cheesebaron.MvxPlugins.SMS.WindowsPhoneStore
+{
+    public class Plugin : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterType<ISmsTask, SmsTask>();
+        }
+    }
+}
