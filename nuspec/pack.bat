49ab6e1a3d8eb5013ef8b6d8ca043c987a13285d
Tomasz Cielecki <tomasz@ostebaronen.dk>
2015-11-09 16:18:02 +0100

    Updated pack script

--- nuspec/pack.bat
+++ nuspec/pack.bat
@@ -1,12 +1,11 @@
 del *.nupkg
-nuget setapikey ---
+nuget setapikey
 
 nuget pack Cheesebaron.MvxPlugins.AppId.nuspec -symbols
-nuget pack Cheesebaron.MvxPlugins.Sms.nuspec -symbols
+nuget pack Cheesebaron.MvxPlugins.DeviceInfo.nuspec -symbols
 nuget pack Cheesebaron.MvxPlugins.Settings.nuspec -symbols
 nuget pack Cheesebaron.MvxPlugins.SimpleWebToken.nuspec -symbols
-nuget pack Cheesebaron.MvxPlugins.FormsPresenters.nuspec -symbols
-nuget pack Cheesebaron.MvxPlugins.Notifications.nuspec -symbols
+nuget pack Cheesebaron.MvxPlugins.Sms.nuspec -symbols
 
 for /r %%i in (*.nupkg) do (call :pushpackage "%%i")
 pause
