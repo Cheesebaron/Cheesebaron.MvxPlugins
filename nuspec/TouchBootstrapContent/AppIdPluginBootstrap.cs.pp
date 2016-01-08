8dbaf9f7e301ddd4cc88cfdab10ed76924f73290
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-01-08 10:47:28 +0100

    Fix appid bootstrap

--- nuspec/TouchBootstrapContent/AppIdPluginBootstrap.cs.pp
+++ nuspec/TouchBootstrapContent/AppIdPluginBootstrap.cs.pp
@@ -3,5 +3,5 @@ using Cirrious.CrossCore.Plugins;
 namespace $rootnamespace$.Bootstrap
 {
     public class AppIdPluginBootstrap
-        : MvxLoaderPluginBootstrapAction<Cheesebaron.MvxPlugins.AppId.PluginLoader, Cheesebaron.MvxPlugins.AppId.Touch.Plugin> { }
+        : MvxLoaderPluginBootstrapAction<Cheesebaron.MvxPlugins.AppId.PluginLoader, Cheesebaron.MvxPlugins.AppId.Plugin> { }
 }
