be8ee425fa597b9c0c3dc1c44eb4de8f5459a870
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-06-13 10:48:53 +0200

    Get better windows version

--- DeviceInfo/WindowsCommon/WindowsCommonDeviceInfo.cs
+++ DeviceInfo/WindowsCommon/WindowsCommonDeviceInfo.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration.Pnp;
 using Windows.Graphics.Display;
 using Windows.Security.ExchangeActiveSyncProvisioning;
 
@@ -57,5 +61,39 @@ namespace Cheesebaron.MvxPlugins.DeviceInfo.WindowsCommon
         }
 
         public DeviceType DeviceType => DeviceType.Windows;
+
+        private const string DeviceDriverKey = "{A8B865DD-2E3D-4094-AD97-E593A70C75D6}";
+        private const string DeviceDriverVersionKey = DeviceDriverKey + ",3";
+        private const string DeviceDriverProviderKey = DeviceDriverKey + ",9";
+        private const string RootContainer = "{00000000-0000-0000-FFFF-FFFFFFFFFFFF}";
+        private const string RootContainerQuery = "System.Devices.ContainerId:=\"" + RootContainer + "\"";
+
+        public static async Task<string> GetWindowsVersionAsync()
+        {
+            // There is no good place to get this so we're going to use the most popular
+            // Microsoft driver version number from the device tree.
+            var requestedProperties = new[] { DeviceDriverVersionKey, DeviceDriverProviderKey };
+
+            var microsoftVersionedDevices = (await PnpObject.FindAllAsync(PnpObjectType.Device, requestedProperties, RootContainerQuery))
+                .Select(d => new {
+                    Provider = (string)GetValueOrDefault(d.Properties, DeviceDriverProviderKey),
+                    Version = (string)GetValueOrDefault(d.Properties, DeviceDriverVersionKey)
+                })
+                .Where(d => d.Provider == "Microsoft" && d.Version != null)
+                .ToList();
+
+            var versionNumbers = microsoftVersionedDevices
+                .GroupBy(d => d.Version.Substring(0, d.Version.IndexOf('.', d.Version.IndexOf('.') + 1)))
+                .OrderByDescending(d => d.Count())
+                .ToList();
+
+            return versionNumbers.Count > 0 ? versionNumbers[0].Key : "";
+        }
+
+        private static TValue GetValueOrDefault<TKey, TValue>(IReadOnlyDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            TValue value;
+            return dictionary.TryGetValue(key, out value) ? value : default(TValue);
+        }
     }
 }
