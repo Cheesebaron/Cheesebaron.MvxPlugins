89f49db0afea2a208568c8ae2a98448db82da366
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-08-22 22:17:26 +0200

    Remove old scripts

--- switchtosrc.ps1
+++
@@ -1,8 +0,0 @@
-$binPath='submodules\MvvmCross-Binaries\VS2012\bin'
-$srcPath='submodules\MvvmCross\bin'
-
-ls *.csproj -rec | 
-	%{ $f=$_; (gc $f.PSPath) | 
-		%{ $_ -replace [regex]::Escape($binPath), $srcPath } | 
-		sc $f.PSPath 
-	}
