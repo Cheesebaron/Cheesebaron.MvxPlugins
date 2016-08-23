82aaf0c76b47a9603f36956ec268638f61827cd6
Tomasz Cielecki <tomasz@ostebaronen.dk>
2016-08-23 22:56:24 +0200

    Fix simplewebtoken plugin after removing winphone

---
+++ SimpleWebToken/Cheesebaron.MvxPlugins.SimpleWebToken.Console/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using Cheesebaron.MvxPlugins.SimpleWebToken.Interfaces;
+using MvvmCross.Platform;
+using MvvmCross.Platform.Plugins;
+
+namespace Cheesebaron.MvxPlugins.SimpleWebToken
+{
+    public class Plugin
+        : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterType<ISimpleWebToken, SimpleWebToken>();
+        }
+    }
+}
