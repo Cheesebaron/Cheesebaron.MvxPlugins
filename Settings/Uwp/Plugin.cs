49c44484d14ae8f127a7c4d89202c4a6a8dc4708
Tomasz Cielecki <tomasz@ostebaronen.dk>
2017-03-08 23:12:49 +0100

    UWP Settings

---
+++ Settings/Uwp/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using Cheesebaron.MvxPlugins.Settings.Interfaces;
+using MvvmCross.Platform;
+using MvvmCross.Platform.Plugins;
+
+namespace Cheesebaron.MvxPlugins.Settings.Uwp
+{
+    public class Plugin
+        : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterType<ISettings, WindowsUwpSettings>();
+        }
+    }
+}
