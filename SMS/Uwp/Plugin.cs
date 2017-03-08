f7c91f8e0cb0ad96385ce5f1519edbcbf139ff1d
Tomasz Cielecki <tomasz@ostebaronen.dk>
2017-03-08 22:26:24 +0100

    Added SMS WindowsUWP

---
+++ SMS/Uwp/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using MvvmCross.Platform;
+using MvvmCross.Platform.Plugins;
+
+namespace Cheesebaron.MvxPlugins.SMS.WindowsUwp
+{
+    public class Plugin : IMvxPlugin
+    {
+        public void Load()
+        {
+            Mvx.RegisterType<ISmsTask, SmsTask>();
+        }
+    }
+}
